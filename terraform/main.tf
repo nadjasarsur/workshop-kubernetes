e7c79b0343f5bb109417824e0eae6ededf0a283b
Nadja Sarsur <nsarsur@thoughtworks.com>
2017-07-05 11:45:19 -0300

    Automate cluster deployment on GPC with Terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,24 @@
 provider "google" {
-  credentials = ""
-  project      = ""
+  project      = "techops-infradel"
   region       = "europe-west1"
+}
+
+resource "google_container_cluster" "primary" {
+  name               = "workshop-kubernetes-staffing"
+  zone               = "europe-west1-c"
+  initial_node_count = 1
+
+  additional_zones = [
+    "europe-west1-d",
+  ]
+
+  node_config {
+    machine_type = "n1-standard-2"
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/compute",
+      "https://www.googleapis.com/auth/devstorage.read_only",
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
+    ]
+  }
 }
