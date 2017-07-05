a36bfc160e36803b40a75be6d69b858ac1ad88eb
Nadja Sarsur <nsarsur@thoughtworks.com>
2017-07-05 14:35:44 -0300

    Update deploy.sh script

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
 docker-compose build
+cd terraform
+terraform plan -out infrastructure.plan
+terraform apply
+gcloud container clusters get-credentials workshop-kubernetes-staffing --zone=europe-west1-c
+kubectl get pods
