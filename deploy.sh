af719e7b82b47cc66d77e4feaba789911f314369
Nadja Sarsur <nsarsur@thoughtworks.com>
2017-07-06 10:59:13 -0300

    Improve deploy.sh script

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 
-docker-compose build
-cd terraform
-terraform plan -out infrastructure.plan
+cd terraform; terraform destroy -force
+
+cd ..; docker-compose build
+docker tag workshop gcr.io/techops-infradel/workshop
+gcloud docker -- push gcr.io/techops-infradel/workshop
+
+cd terraform; terraform plan -out infrastructure.plan
 terraform apply
 gcloud container clusters get-credentials workshop-kubernetes-staffing --zone=europe-west1-c
-kubectl get pods
+
+cd ../; kubectl create -f kubernetes/deployment.yml
+kubectl expose deployment workshop-kubernetes --type=LoadBalancer
+
+echo """
+Run the following to open up the application in your browser
+EXTERNAL_IP=$(kubectl describe services workshop-kubernetes | grep "LoadBalancer Ingress" | cut -f2)
+open http://$EXTERNAL_IP:9000
+"""
