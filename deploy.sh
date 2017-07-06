229c9ef36ad55a5e3095db4fdd2a0b02c4af8356
Nadja Sarsur <nsarsur@thoughtworks.com>
2017-07-06 17:49:39 -0300

    Update deploy script

--- deploy.sh
+++ deploy.sh
@@ -3,18 +3,21 @@
 cd terraform; terraform destroy -force
 
 cd ..; docker-compose build
-docker tag workshop gcr.io/techops-infradel/workshop
-gcloud docker -- push gcr.io/techops-infradel/workshop
+docker tag web gcr.io/techops-infradel/web
+gcloud docker -- push gcr.io/techops-infradel/web
 
 cd terraform; terraform plan -out infrastructure.plan
 terraform apply
 gcloud container clusters get-credentials workshop-kubernetes-staffing --zone=europe-west1-c
 
-cd ../; kubectl create -f kubernetes/deployment.yml
-kubectl expose deployment workshop-kubernetes --type=LoadBalancer
+cd ../
+kubectl create -f kubernetes/redis-deployment.yml
+kubectl create -f kubernetes/redis-service.yml
+kubectl create -f kubernetes/web-deployment.yml
+kubectl create -f kubernetes/web-service.yml
 
 echo """
 Run the following to open up the application in your browser
-EXTERNAL_IP=$(kubectl describe services workshop-kubernetes | grep "LoadBalancer Ingress" | cut -f2)
+EXTERNAL_IP=\$(kubectl describe services workshop-kubernetes-web | grep "LoadBalancer Ingress" | cut -f2)
 open http://$EXTERNAL_IP:9000
 """
