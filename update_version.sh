d6eed023d2e5ec5b83cba49c3055321ccbc1ef96
Lucas Cunha <lmcunha@thoughtworks.com>
2017-07-07 13:22:09 -0300

    Add update version scripts

---
+++ update_version.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+WEB_VERSION="${1:-latest}"
+echo "Generating version: $WEB_VERSION"
+
+docker-compose build web
+docker tag web gcr.io/techops-infradel/web:$WEB_VERSION
+gcloud docker -- push gcr.io/techops-infradel/web:$WEB_VERSION
+
+kubectl set image deployment/workshop-kubernetes-web web=gcr.io/techops-infradel/web:$WEB_VERSION
+
+kubectl rollout status deployment/workshop-kubernetes-web
